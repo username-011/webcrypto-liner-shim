32cf9938373a6eec2ffa15a4963865db0f187e46
kurenai-11 <moldrenkailich@gmail.com>
2025-12-22 17:34:42 +0300

    new build

--- src/shim.ts
+++ src/shim.ts
@@ -11,11 +11,16 @@ if (nativeCrypto) {
 try {
   // Replace original crypto by liner if needed
   if (window) {
-    console.log("Liner runtime: checking crypto...");
+    console.log("[webcrypto-liner] Checking crypto...");
     if (window.crypto === "undefined" || window.crypto.subtle === undefined) {
+      console.log(
+        "[webcrypto-liner] Native crypto is not available. Using webcrypto-liner shim."
+      );
       delete (self as any).crypto;
       window.crypto = new Crypto();
       Object.freeze(window.crypto);
+    } else {
+      console.log("[webcrypto-liner] Native crypto is available.");
     }
   }
 } catch (e) {
